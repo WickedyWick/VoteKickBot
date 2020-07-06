6c2e6f5548439df0edef14608239704f5a55afbf
WickedyWick <49614957+WickedyWick@users.noreply.github.com>
2020-07-06 10:00:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 // require the discord.js module
 const Discord = require('discord.js');
 
+//// TODO 
+
+
 // create a new Discord client
 const client = new Discord.Client();
 const myID = '140843716016472065';
 const botID = '729339089136123995';
-const allowedToBeVotedIDS = ['378674509236142081','682027458362736678','441289380091527169','292619657289007115','426372379590131712','152129574380109824','315217012500463617','320539184257564672'];
-const allowedIDS = ['378674509236142081','682027458362736678','140843716016472065','441289380091527169','292619657289007115','426372379590131712','152129574380109824','315217012500463617','320539184257564672'];
+const allowedToBeVotedIDS = ['378674509236142081','682027458362736678','441289380091527169','292619657289007115','426372379590131712','152129574380109824','315217012500463617','320539184257564672','398871074760163328','324260746261299200'];
+const allowedIDS = ['378674509236142081','682027458362736678','140843716016472065','441289380091527169','292619657289007115','426372379590131712','152129574380109824','315217012500463617','320539184257564672','324260746261299200'];
+let alreadyVoted = [];
 let personToKickID = '';
 let inFavor = 0;
 let against =0;
 let numberOfVotes = 0;
-const minNumOfVotes = 5;
+const minNumOfVotes = 2;
 let votingInProgress = false;
 let VoteStarted = false;
 // when the client is ready, run this code
@@ -19,24 +23,32 @@ let VoteStarted = false;
 client.once('ready', () => {
 	console.log('Ready!');
 });
-let personToKick = new Discord.GuildMember();
+var personToKick = new Discord.GuildMember('','','');
 client.on('message', message =>{
     //VoteKick start
     if(message.content.startsWith('!VoteKick') && !VoteStarted){
         //person to be kicked
         personToKick = message.guild.member(message.mentions.users.first()); 
-        //If i'm the one being kicked
-        if(personToKick.id === myID){
-            message.reply('You can\' kick my dad');
-        //If the bot is the one being kicked
-        }else if(personToKick.id === botID){
-            message.reply('You must have been thinking how smart you are couple of seconds ago, retard');
-        }else if(allowedToBeVotedIDS.includes(personToKick.id)){
-            if(!VoteStarted){
-                personToKickID = personToKick.id;
-                message.reply('Voting started for ' + personToKick.displayName + '\nMinimum Votes needed :5 \n People that are able to vote : Dkay, Hotpants, Retard Chrome, Truva, Trans , Dar , Kendall  and WIck \n For Yes write \'!Vote +\'  and for No write \'!Vote -\'');               
-                VoteStarted = true;
+        console.log('IT GOT HERE')
+        //da proveri da li osoba moze da se kikuje
+        if(personToKick.kickable){
+              //If i'm the one being kicked
+            if(personToKick.id === myID){
+                message.reply('You can\'t kick my dad');
+            //If the bot is the one being kicked
+            }else if(personToKick.id === botID){
+                message.reply('You must have been thinking how smart you are couple of seconds ago, retard');
+            }else if(allowedToBeVotedIDS.includes(personToKick.id)){
+                if(!VoteStarted){
+                    personToKickID = personToKick.id;
+                    message.reply('Voting started for ' + personToKick.displayName + '\nMinimum Votes needed :5 \n People that are able to vote : Dkay, Hotpants, Retard Chrome, Truva, Trans , Dar , Kendall  and WIck \n For Yes write \'!Vote +\'  and for No write \'!Vote -\'');               
+                    VoteStarted = true;
+                }
+            }else if(!allowedToBeVotedIDS.includes(personToKick.id)){
+                message.reply('Only Dkay, Hotpants, Retard Chrome, Truva, Trans , Dar , Kendall  and WIck are allowed to participate!');
             }
+        }else{
+            message.reply('This person is not eligble to be kicked due permissions difference!');
         }
         //if VoteKick is in progresse
     }else if(message.content.startsWith('!VoteKick') && VoteStarted){
@@ -47,19 +59,26 @@ client.on('message', message =>{
         let msg = message.content.trim();
         //so you cant vote in your own case
         if(message.member.id === personToKickID){
-            message.reply('You can\' vote for your case!');
+            message.reply('You can\'t vote for your case!');
             //so only allowed ones can vote
         }else if(!allowedIDS.includes(message.member.id)){
             message.reply('You are not allowed to vote');
         }
         //counting votes
         else{
-            if(msg === '!Vote +'){
-                inFavor ++;               
-            }else if(msg === '!Vote -'){
-                against++;               
+            if(!alreadyVoted.includes(message.member.id)){
+                if(msg === '!Vote +'){
+                    inFavor ++;       
+                    message.reply("Vote successfull!");
+                }else if(msg === '!Vote -'){
+                    against++;  
+                    message.reply("Vote successfull!");             
+                }
+                numberOfVotes++;
+                alreadyVoted.push(message.member.id);
+            }else{
+                message.reply('You can vote only once!');
             }
-            numberOfVotes++;
         }
     //To Vote if votekick progress hasn't started
     }else if(message.content.startsWith("!Vote") && !VoteStarted){
@@ -71,20 +90,21 @@ client.on('message', message =>{
             if(numberOfVotes >= minNumOfVotes){
                 // za > protiv
                 if(inFavor > against){
-                    message.reply("VoteKick finished! In favor : "+ String(inFavor) + " , Against : " + String(against) + "\n" + personToKick.displayName + "IS ABOUT TO GET KICKED");
+                    message.reply("VoteKick finished! In favor : "+ String(inFavor) + " , Against : " + String(against) + "\n" + personToKick.displayName + " IS ABOUT TO GET KICKED");
                     personToKick.kick("YOU HAVE BEEN VOTED OFF");
                 }else if( inFavor == against){
                     message.reply("VoteKick finished! In favor : "+ String(inFavor) + " , Against : " + String(against) + "\n DRAW");
                 }else if(inFavor < against){
-                    message.reply("VoteKick finished! In favor : "+ String(inFavor) + " , Against : " + String(against) + "\n"+personToKick.displayName + "ISN\'T GETTING KICKED");
+                    message.reply("VoteKick finished! In favor : "+ String(inFavor) + " , Against : " + String(against) + "\n"+personToKick.displayName + " ISN\'T GETTING KICKED");
                 }
-
+                inFavor = 0;
+                against = 0;
+                numberOfVotes =0;
+                VoteStarted = false;
             }else{
                 message.reply('Not Enough Votes');
             }
-            inFavor = 0;
-            against = 0;
-            numberOfVotes =0;
+            
         }else{
             message.reply('Only Master Wick can request results!');
         }
@@ -97,4 +117,4 @@ client.on('message', message =>{
 
 });
 // login to Discord with your app's token
-client.login('token');
+client.login('NzI5MzM5MDg5MTM2MTIzOTk1.XwIQjg.sZlFmmmRcyxfoZGDbrFPxhoC50c');
